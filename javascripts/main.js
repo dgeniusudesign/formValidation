f255f96591fba25e5271f81c7283a1cbe4468007
Monish Kumar <dgeniusudesign@gmail.com>
2015-05-18 23:41:44 -0500

    Small validations

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 function showValues() {
-  
+  var fN = $("#firstNameText").val();
+  var lN = $("#lastNameText").val();
+  $("#result").text(fN+lN)
 }
+
 $(document).ready(function() {
   $("#firstNameText").on('focusout', function() {
     if( $(this).val() == "" ) {
@@ -8,6 +11,48 @@ $(document).ready(function() {
     }
     else {
       $("#firstNameErr").text("");
+      $("#firstNameText").addClass("initCap");
+    }
+  });
+  $("#lastNameText").on('focusout', function() {
+    if( $(this).val() == "" ) {
+      $("#lastNameErr").text("Last Name cannot be empty!");
+    }
+    else {
+      $("#lastNameErr").text("");
+      $("#lastNameText").addClass("initCap");
+    }
+  });
+  $("#emailText").on('focusout', function() {
+    if( $(this).val() == "" ) {
+      $("#emailErr").text("Email cannot be empty!");
+    }
+    else {
+      $("#emailErr").text("");
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;;
+      console.log($("#emailText").val());
+      console.log(re.test($("#emailText").val()));
+      if(!re.test($("#emailText").val())) {
+        $("#emailErr").text("Email isn't valid! Enter a proper Email ID!");
+      } else {
+        $("#emailErr").text("");
+      }
+    }
+  });
+  $("#phoneText").on('focusout', function() {
+    if( $(this).val() == "" ) {
+      $("#phoneErr").text("Phone cannot be empty!");
+    }
+    else {
+      $("#phoneErr").text("");
+      var re = /^\d{10}$/;
+      console.log($("#phoneErr").val());
+      console.log(re.test($("#phoneText").val()));
+      if(!re.test($("#phoneText").val())) {
+        $("#phoneErr").text("Phone Number isn't valid! Enter a proper number");
+      } else {
+        $("#phoneErr").text("");
+      }
     }
   });
 });
